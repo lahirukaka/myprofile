7898bccb5b25fcb704768b45d513c5ba212cf00a
lahirukaka <lahirukaka@gmail.com>
2015-10-23 17:53:26 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Lahiru Udana</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="styles/styles-large.css" />
+        <script src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+<?php
+    require_once("ui/sidebar.php");
+    require_once("ui/content.php");
+?>
+    </body>
+</html>
---
+++ ui/sidebar.php
@@ -0,0 +1,14 @@
+<section id="sidebar">
+    <figure id="face">
+        <img src="res/face.png" />
+    </figure>
+    <h1 id="name">Lahiru Udana</h1>
+    <h4 id="titles">( <span title="Cisco Certified Network Associate">CCNA</span> | <span title="Cisco Certified Security Associate">CCNAS</span> | <span title="">VTSP</span> )</h4>
+    <article id="social">
+    </article>
+    <nav>
+        <a class="active">About</a>
+        <a>Education</a>
+        <a>Contact</a>
+    </nav>
+</section>
