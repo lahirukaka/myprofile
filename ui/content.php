b39256764458f9b6d571e71c18aeead4c8e42dd7
Lahiru <lahirukaka@gmail.com>
2016-03-18 18:23:36 +0530

    added social icons

--- ui/content.php
+++ ui/content.php
@@ -1,3 +1,3 @@
 <section id="content">
-    
+
 </section>
--- ui/sidebar.php
+++ ui/sidebar.php
@@ -5,6 +5,14 @@
     <h1 id="name">Lahiru Udana</h1>
     <h4 id="titles">( <span title="Cisco Certified Network Associate">CCNA</span> | <span title="Cisco Certified Security Associate">CCNAS</span> | <span title="">VTSP</span> )</h4>
     <article id="social">
+        <div class="full-inner-wrapper">
+            <a target="_blank" title="Facebook Profile" href="https://www.facebook.com/colhh"><img class="social-icons" src="res/facebook.png" /></a>
+            <a target="_blank" title="Google Plus Profile" href="https://plus.google.com/+LahiruUdanaColhh"><img class="social-icons" src="res/google_plus.png" /></a>
+            <a target="_blank" title="LinkedIn Profile" href="https://www.linkedin.com/in/lahirukaka"><img class="social-icons" src="res/linkedin_logo.png" /></a>
+            <a target="_blank" title="GIthub Profile" href="https://github.com/lahirukaka"><img class="social-icons" src="res/github.png" /></a>
+            <a target="_blank" title="Blogger Profile" href="https://www.blogger.com/profile/02207073594107436871"><img class="social-icons" src="res/blogger.png" /></a>
+            <a target="_blank" title="Twitter Profile" href="https://twitter.com/lahirukaka"><img class="social-icons" src="res/twitter.png" /></a>
+        </div>
     </article>
     <nav>
         <a class="active">About</a>
