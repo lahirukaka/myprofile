4ccb4dd3afc73dc8cc0d9bee810d40e810a9684e
lahirukaka <lahirukaka@gmail.com>
2016-03-19 22:08:16 +0530

    adding js to sidebar

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ date_default_timezone_set( "Asia/Colombo" )
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <link rel="stylesheet" href="styles/main.css" />
         <link rel="stylesheet" href="styles/styles-large.css" />
-        <script src="js/jquery-2.1.4.min.js"></script>
+        <link rel="stylesheet" href="styles/animate.css" />
+        <script src="js/jquery-2.2.2.min.js"></script>
         <script src="js/script.js"></script>
     </head>
     <body>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,51 @@
 $(function(){
-    
+    /*events*/
+    $('#face img').hover(shakeProfilePicture);
+    $("#social > div.full-inner-wrapper").hover(flipSocialIcons);
+    /*sidebar*/
+    shakeProfilePicture();
 });
+
+/*functions*/
+function shakeProfilePicture()
+{
+    $ele=$('#face img');
+    $cls="animated swing";
+    $ele.addClass($cls);
+    doWhenDone($ele,function(){$ele.removeClass($cls);shakeName();flashTitles();});
+}
+
+function shakeName()
+{
+    $ele=$("#name");
+    $cls="animated shake";
+    $ele.addClass($cls);
+    doWhenDone($ele,function(){$ele.removeClass($cls);});
+}
+
+function flashTitles()
+{
+    $ele=$("#titles");
+    $cls="animated flash";
+    $ele.addClass($cls);
+    doWhenDone($ele,function(){$ele.removeClass($cls);});
+}
+
+function flipSocialIcons()
+{
+    $ele=$('#social img.social-icons');
+    $cls="animated rotateIn";
+    $time=200;
+    $ele.each(function(i,e){
+        $(e).addClass($cls).delay(500);
+        doWhenDone($(e),function(){
+            $(e).removeClass($cls);
+        });
+    });
+}
+
+/*common*/
+function doWhenDone($ele,$func)
+{
+    $ele.on('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', $func);
+}
