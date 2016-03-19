f0e60555c668d2475c05e5dc8516a8844a20c929
lahirukaka <lahirukaka@gmail.com>
2016-03-19 17:45:31 +0530

    completed designing contact page

--- ui/contact.php
+++ ui/contact.php
@@ -1,12 +1,36 @@
 <article id="contact" class="page">
     <section class="inner-wrapper">
-        <div class="contact-item">a</div>
-        <div class="contact-item">b</div>
-        <div class="contact-item">c</div>
-        <div class="contact-item">d</div>
-        <div class="contact-item">e</div>
-        <div class="contact-item">f</div>
-        <div class="contact-item">g</div>
-        <div class="contact-item">h</div>
+        <div data-url="https://www.facebook.com/colhh" class="contact-item">
+            <img src="res/facebook-large.png" />
+            <div>Facebook</div>
+        </div>
+        <div class="contact-item" data-url="https://www.linkedin.com/in/lahirukaka">
+            <img src="res/linkedin-large.png" />
+            <div>LinkedIn</div>
+        </div>
+        <div class="contact-item" data-url="https://plus.google.com/+LahiruUdanaColhh">
+            <img src="res/googleplus-large.png" />
+            <div>Google+</div>
+        </div>
+        <div class="contact-item" data-url="https://github.com/lahirukaka">
+            <img src="res/github-large.png" />
+            <div>Github</div>
+        </div>
+        <div class="contact-item" data-url="https://www.blogger.com/profile/02207073594107436871">
+            <img src="res/blogger-large.png" />
+            <div>Blogger</div>
+        </div>
+        <div class="contact-item" data-url="https://twitter.com/lahirukaka">
+            <img src="res/twitter-large.png" />
+            <div>Twitter</div>
+        </div>
+        <div class="contact-item" data-email="lahirukaka@gmail.com">
+            <img src="res/gmail-large.png" />
+            <div>Gmail</div>
+        </div>
+        <!--div class="contact-item">
+            <img src="" />
+            <span></span>
+        </div-->
     </section>
 </article>
